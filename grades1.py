24481015143ef08c2add53ae8980e1e3aee21e27
Nicole Choi <minjungchoi.is@gmail.com>
2013-08-12 23:07:09 -0400

    yaho

---
+++ grades1.py
@@ -0,0 +1,46 @@
+#when variable is an integer
+
+num_of_tests = 7
+
+student1 = 98 + 100 + 76 + 85 + 99
+student2 = 56 + 76 + 85 + 100 + 72
+student3 = 95 + 80 + 76 + 68 + 52
+
+print "Student one: "
+print student1 / num_of_tests
+print "Student two: "
+print student2 / num_of_tests
+print "Student three: "
+print student3 / num_of_tests, '\n'
+
+
+#when variable is a float
+
+num_of_tests = 7.0
+
+student1 = 98 + 100 + 76 + 85 + 99
+student2 = 56 + 76 + 85 + 100 + 72
+student3 = 95 + 80 + 76 + 68 + 52
+
+print "Student one: "
+print student1 / num_of_tests
+print "Student two: "
+print student2 / num_of_tests
+print "Student three: "
+print student3 / num_of_tests, '\n'
+
+
+#when variable is a float (2)
+
+num_of_tests = 7.00
+
+student1 = 98.00 + 100.00 + 76.0 + 85.0 + 99.00
+student2 = 56.00 + 76.00 + 85.00 + 100.00 + 72.00
+student3 = 95.00 + 80.00 + 76.0 + 68.00 + 52.00
+
+print "Student one: "
+print student1 / num_of_tests
+print "Student two: "
+print student2 / num_of_tests
+print "Student three: "
+print student3 / num_of_tests
