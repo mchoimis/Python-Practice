b087f02f92dbf71ba3ac8759b7e877695e2fce77
Nicole Choi <minjungchoi.is@gmail.com>
2013-08-28 22:41:00 -0400

    update

--- 20130828_wikipedia-scraper2.py
+++ 20130828_wikipedia-scraper2.py
@@ -1,6 +1,10 @@
 # This is not working
 
-
+from bs4 import  BeautifulSoup
+import urllib2
+import re
+import nltk
+import json
 import wikipedia
 
 page = wikipedia.Page(wikipedia.getSite(), 'Tom_Cruise')
