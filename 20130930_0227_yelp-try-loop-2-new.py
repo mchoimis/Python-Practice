a83a0374e44ba915221a1c4d3d3491aab8e588f1
Nicole Choi <minjungchoi.is@gmail.com>
2013-10-07 15:30:16 -0400

    update

--- 20130930_0227_yelp-try-loop-2-new.py
+++ 20130930_0227_yelp-try-loop-2-new.py
@@ -3,9 +3,15 @@ import sys
 import httplib2
 from BeautifulSoup import BeautifulSoup
 
-conn = httplib2.Http(".cache-Philadelphia-PA-20131007")
+conn = httplib2.Http(".cache-20131007")
 url = u"http://www.yelp.com"
-page = conn.request(url + '/browse/reviews/picks?edition_id=hJ2dg0kOHm31nGAH-7zP6A&start=',"GET")
+page = conn.request(url + '/browse/reviews/picks?edition_id=hJ2dg0kOHm31nGAH-7zP6A&start=',"GET") #Philly
+page = conn.request(url + '/browse/reviews/picks?edition_id=9549vDBw7ab0cuc930BQ1g&start=',"GET") #Houston
+page = conn.request(url + '/browse/reviews/picks?edition_id=sE3ge33huDcNJGW3V4obww&start=',"GET") #Las Vegas
+page = conn.request(url + '/browse/reviews/picks?edition_id=jQEQQKOmCe0oTia-6F6Pyw&start=',"GET") #Loisville
+page = conn.request(url + '/browse/reviews/picks?edition_id=c6HT44PKCaXqzN_BdgKPCw&start=',"GET") #San Fran
+page = conn.request(url + '/browse/reviews/picks?edition_id=RU7bd3h2f6pBlf8BfAyxGQ&start=',"GET") #nyc manhattan
+page = conn.request(url + '/browse/reviews/picks?edition_id=O0nmI3IrPyK_jhDK6spZUQ&start=',"GET") #st paul 
 
 # page[1] contains the retrieved html.
 
